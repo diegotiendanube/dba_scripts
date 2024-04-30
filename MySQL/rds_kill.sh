660f59e6c2bc3c9d632cf56d7d4aa62311e6a557
diegotiendanube <163137290+diegotiendanube@users.noreply.github.com>
2024-04-30 17:10:33 -0300

    Update rds_kill.sh

--- MySQL/rds_kill.sh
+++ MySQL/rds_kill.sh
@@ -2,6 +2,7 @@
 
 # Arquivo contendo as configurações de conexão
 CONNECTION_LIST="lista_conexoes.txt"
+
 # Configuração de login para o banco de destino
 DEST_CONFIG="data-dba"
 
@@ -10,11 +11,12 @@ CSV_FILE="session_kill.csv"
 
 # Loop através das configurações de conexão no arquivo
 while IFS= read -r config; do
-    # Executar a procedure com a conexão atual
-    #mysql --login-path="$config" -e "CALL monitoring.sp_monitoring_kill_rds('dba_crontab,root,redash_appl', 1);" | awk 'NR>1' > $CSV_FILE
-    mysql --login-path="$config" -e "call monitoring.sp_monitoring_kill_rds('dba_crontab,root,redash_appl', 1);" | awk 'BEGIN {FS="\t"} {print $1","$2","$3","$4","$5","$6","$7","$8","$9","$10","$11","$12}' > $CSV_FILE
+    # Extrair o texto da segunda posição, separando por ":"
+    endpoint=$(echo "$config" | cut -d ":" -f 2)
+    # Executar a procedure com a conexão atual e salvar o resultado em um arquivo CSV
+    mysql --login-path=dba_crontab -h "$endpoint" -e "call monitoring.sp_monitoring_kill_rds('dba_crontab,root,redash_appl', 1);" | awk -v endpoint="$endpoint" 'BEGIN {FS="\t"} {print endpoint","$1","$2","$3","$4","$5","$6","$7","$8","$9","$10","$11","$12}' > $CSV_FILE
 
-    # Verifica se o arquivo CSV tem mais de uma linha
+    # Verificar se o arquivo CSV tem mais de uma linha
     if [ $(wc -l < $CSV_FILE) -gt 1 ]; then
         # Abre uma nova conexão com o banco de destino e insere os dados na tabela history_kill_rds
         while IFS=',' read -r rds_aws_name execution_date execution_status processlist_id thread_id user_name host_name data_base_name execution_time tx_query sql_state erro_number text_information; do
@@ -22,6 +24,6 @@ while IFS= read -r config; do
         done < <(tail -n +2 $CSV_FILE)
     fi
 
-    # Deleta o arquivo CSV
+    # Deletar o arquivo CSV
     rm $CSV_FILE
 done < "$CONNECTION_LIST"
